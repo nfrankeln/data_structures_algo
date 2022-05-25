cfc3c8edbc2e79f2323bc0169f23d8a08b7437bf
Raphael Serota <raphael@codeplatoon.org>
2022-05-25 08:05:44 -0500

    fixed method args

--- python/stack.py
+++ python/stack.py
@@ -1,11 +1,11 @@
 class Stack:
   # write your __init__ method here that should store a 'total' value which is the total number of elements in the Stack and a 'stack' value which is an array of stored values in the Stack
 
-  def push(self):
+  def push(self, data):
     # write your code to add data following LIFO and return the Stack
     pass
 
-  def pop(self, data):
+  def pop(self):
     # write your code to removes the data following LIFO and return the Stack
     pass
 
